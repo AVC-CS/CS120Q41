f2abc7af6349a6da570aa60479564ecfcf892e83
Kyuwoong Lee <kwooler@gmail.com>
2023-09-02 17:14:50 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//      **************************************************
+//      Find the min, max among three values
+//      Display three values in ascending order  min, other, max
+//     **************************************************
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3;
+    int min, max, med;
+    cout << "Enter three integer values: ";
+    cin >> num1 >> num2 >> num3;
+
+    /*
+    Code your program here
+    */
+
+    cout << min << med << max << endl;
+}
